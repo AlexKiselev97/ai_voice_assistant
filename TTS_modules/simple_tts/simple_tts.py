48dc71ffb3b6d54837473b4885f0acf727b95a38
Alexander Kiselev <alexkiselev333@gmail.com>
2025-03-30 22:18:48 +0500

    fix ru voice for simple tts

--- TTS_modules/simple_tts/simple_tts.py
+++ TTS_modules/simple_tts/simple_tts.py
@@ -9,11 +9,14 @@ def text_to_speech(engine, text, lang, is_running):
 def get_voice_engine(lang):
     print('Loading voice engine...')
     engine = pyttsx3.init(driverName='sapi5' if os.name == "win" else "espeak")
-    #voices = engine.getProperty('voices')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[102].id if lang == 'ru' else voices[23].id) 
     #if os.name == "win":
-    #    engine.setProperty('voice', voices[0 if lang == 'ru-RU' else 1].id)
-    #else:
-        #engine.setProperty('voice', voices[62].id if lang == 'ru-RU' else voices[11].id) 
+    #    engine.setProperty('voice', voices[0 if lang == 'ru' else 1].id)
     time.sleep(5)
     print('Done!')
     return engine
+
+if __name__ == "__main__":
+    eng = get_voice_engine('ru')
+    text_to_speech(eng, 'Привет, как дела?', 'ru', lambda: False)
